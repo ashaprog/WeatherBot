215428056f62ab770f41e6125d942341c16d0a54
ashaprog <131791336+ashaprog@users.noreply.github.com>
2023-05-16 18:18:38 +0300

    Update WeatherBot.py
    added nice-looking buttons

--- WeatherBot.py
+++ WeatherBot.py
@@ -18,26 +18,30 @@ mgr = owm.weather_manager()
 
 engine = 'text-davinci-003'
 
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id, 'Список команд:\n-Кнопка "Отправить местоположение" отправит вашу геолокацию и бот расскажет вам о погоде в вашем городе, и сам ChatGPT подскажет вам что надеть)\n'
-                                      '-Кнопка /city позволит вам ввести город вручную и узнать ту же информацию)')
 
 @bot.message_handler(commands=['start'])
 def text(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=False)
-    but1 = types.KeyboardButton('/city')
-    but2 = types.KeyboardButton('/help')
-    get_location = types.KeyboardButton("Отправить местоположение", request_location=True)
+    but1 = types.KeyboardButton('Погода☀')
+    but2 = types.KeyboardButton('Помощь🆘')
+    get_location = types.KeyboardButton("📍Отправить геолокацию", request_location=True)
     keyboard.add(get_location, but1, but2)
-    bot.send_message(message.chat.id, "Привет! Чтобы узнать погоду и рекомендацию по одежде от ChatGPT - нажми на кнопку и передай мне свое местоположение или введи /city чтобы ввести название города вручную", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Привет! Чтобы узнать погоду и рекомендацию по одежде от ChatGPT - нажми на кнопку и передай мне свое местоположение или нажми на кнопку 'Погода☀' чтобы ввести название города вручную", reply_markup=keyboard)
+
+
+@bot.message_handler(func=lambda message: message.text == 'Помощь🆘')
+def help(message):
+    if message.text == 'Помощь🆘':
+        bot.send_message(message.chat.id, 'Список команд:\n-Кнопка "Погода☀" отправит вашу геолокацию и бот расскажет вам о погоде в вашем городе, и сам ChatGPT подскажет вам что надеть)\n'
+                         '-Кнопка /city позволит вам ввести город вручную и узнать ту же информацию)')
+
 
 #хендлер команды отправки геолокации
 
 @bot.message_handler(content_types=["location"])
 def locatWeather(message):
     if message.location is not None:
-        bot.send_message(message.chat.id, 'Ищу погоду в вашем городе...')
+        bot.send_message(message.chat.id, '☀Ищу погоду в вашем городе...')
         lat = message.location.latitude
         lon = message.location.longitude
         observation = mgr.weather_at_coords(lat, lon)
@@ -49,14 +53,14 @@ def locatWeather(message):
         wind = w.wind()['speed']
         feel = int(w.temperature('celsius')['feels_like'])
         temp = int(w.temperature('celsius')['temp'])
-        prompt = f"Скажи что одеть в {w.detailed_status} погоду в 1 предложении при температуре {feel} градусов"
+        prompt = f"Скажи что одеть при {feel} градусов. Предложение должно быть в дружеском стиле"
         completion = openai.Completion.create(engine=engine, prompt=prompt, temperature=0.5, max_tokens=1000)
         completionG = completion.choices[0]['text']
         bot.send_message(message.chat.id, f'☀В вашем городе сейчас {w.detailed_status} \n🌡️Температура {temp} градусов. Ощущается как {feel} градусов. 💨Ветер {wind} м/с. {completionG}-Совет от ChatGPT🤓')
 
 #хендлер команды /city
 
-@bot.message_handler(commands=['city'])
+@bot.message_handler(func=lambda message: message.text == 'Погода☀')
 def city(message):
     city = message.text
     bot.send_message(message.chat.id, "🌇Введи название города")
@@ -75,7 +79,8 @@ def cityWeather(message):
                           'rain': 'дождь🌧️', 'thunderstorm': 'гроза🌩️', 'snow': 'идет снег🌨️', 'mist': 'туманно🌫️', 'overcast clouds': 'облачно☁'}
         w.detailed_status = weathercondRus[w.detailed_status]
         feel = int(w.temperature('celsius')['feels_like'])
-        prompt = f"Скажи что одеть в {w.detailed_status} погоду в 1 предложении при температуре {feel} градусов"
+        #prompt = f"Скажи что одеть в {w.detailed_status} погоду в 1 предложении и неформальном стиле при температуре {feel} градусов"
+        prompt = f"Скажи что одеть при {feel} градусов. Предложение должно быть в дружеском стиле"
         completion = openai.Completion.create(engine=engine, prompt=prompt, temperature=0.5, max_tokens=1000)
         completionG = completion.choices[0]['text']
         bot.send_message(message.chat.id,
@@ -91,4 +96,3 @@ def text_handler(message):
     pass
 
 bot.polling(none_stop=True)
-
